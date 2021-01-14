10c73800345ef8ad19b7100054b7cd6cea2a9217
prodigysurge <boatinglife6@yahoo.com>
2021-01-14 13:33:15 -0500

    commit tree c84f7462b6f34080014b106455b55f3142c054d2

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+require "pry"
+
+
+def line(array)
+    if array.length == 0
+        puts "The line is currently empty."
+    else
+        output = "The line is currently:"
+        array.each_with_index do |people, index|
+            no_people = " #{index +1}. #{people}"
+            output = output + no_people
+        
+        end
+        puts output
+    end
+end
+def take_a_number(array, ele)
+    array.push (ele)
+        puts "Welcome, #{ele}. You are number #{array.length} in line."
+    
+end
+
+def now_serving(array)
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array.shift()}."
+    end
+end
+
+
+
+
+
+    
